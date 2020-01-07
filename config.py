6e19883f709ca4b788d749ccf99740a27529e596
Icy <d360478265@163.com>
2020-01-07 17:46:16 +0800

    sync

---
+++ config.py
@@ -0,0 +1,42 @@
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = '8cOgMVUSY3wPWCLkzwSlEKEU1a'
+    MAIL_SERVER = 'smtp.qq.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = 'my@qq.com'
+    MAIL_PASSWORD = 'yourpassword'
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = '360478265@qq.com'
+    FLASKY_ADMIN = 'deng.changchao@yanwei365.com'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI     = 'sqlite: ///' + os.path.join(basedir, 'data.sqlite')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI     = 'sqlite: ///' + os.path.join(basedir, 'data.sqlite')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI     = 'sqlite: ///' + os.path.join(basedir, 'data.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
